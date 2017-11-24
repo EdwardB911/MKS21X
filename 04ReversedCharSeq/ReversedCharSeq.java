a4c23c41a4440f366ebf7b3c42a788fbcca4af9b
EdwardB911 <ebukhman@stuy.edu>
2017-11-24 14:44:45 -0500

    Fixed errors and wrote main, final version

--- 04ReversedCharSeq/ReversedCharSeq.java
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -12,18 +12,27 @@ public class ReversedCharSeq implements CharSequence{
     
     public String subSequence(int start, int end){
 	String str = "";
-	for(int x = data.length() - start; x < data.length() - end; x--){
-	    str = str + data.substring(x, x + 1);
+	for(int x = start; x < end; x++){
+	    str = str + this.charAt(x);
 	}
 	return str;
     }
 
     public char charAt(int index){
-	return data.charAt(data.length() - index);
+	return data.charAt(data.length() - index -1);
     }
 
     public String toString(){
 	return this.subSequence(0, this.length());
-    }	
+    }
+
+    public static void main(String[] args){
+	ReversedCharSeq TEST = new ReversedCharSeq("ABCDEF");
+	System.out.println(TEST.charAt(2));
+	System.out.println(TEST.length());
+	System.out.println(TEST.subSequence(0, 4));
+	System.out.println(TEST.toString());
+	
+    }
     	    
 }
