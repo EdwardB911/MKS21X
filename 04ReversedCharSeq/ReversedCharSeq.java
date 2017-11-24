22f4d77d07fe23a8e82c2deccd9a6109b4381d98
EdwardB911 <ebukhman@stuy.edu>
2017-11-24 14:24:34 -0500

    Wrote methods, first iteration

--- 04ReversedCharSeq/ReversedCharSeq.java
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -1,2 +1,29 @@
-public class ReversedCharSequence implements CharSequence{
+public class ReversedCharSeq implements CharSequence{
+
     private String data;
+
+    public ReversedCharSeq(String str){
+	data = str;     
+    }
+
+    public int length(){
+	return data.length();
+    }	    
+    
+    public String subSequence(int start, int end){
+	String str = "";
+	for(int x = data.length() - start; x < data.length() - end; x--){
+	    str = str + data.substring(x, x + 1);
+	}
+	return str;
+    }
+
+    public char charAt(int index){
+	return data.charAt(data.length() - index);
+    }
+
+    public String toString(){
+	return this.subSequence(0, this.length());
+    }	
+    	    
+}
