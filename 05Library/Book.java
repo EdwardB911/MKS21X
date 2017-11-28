61da723ff7cb77c1da347a63cfc97c73c4a1f927
EdwardB911 <ebukhman@stuy.edu>
2017-11-28 15:26:30 -0500

    Fixed variable

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -2,12 +2,18 @@ public class Book{
     
     private String author;
     private String title;
-    private int ISBN;
+    private String ISBN;
 
     public Book(){
 	
     }
 
-    public Book(String a, 
+    public Book(String a, String t, String i){
+	author = a;
+	title = t;
+	ISBN = i;
+    }
 }
+	    
+	    
 	
