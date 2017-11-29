f4109e6fc80afb28c9ed22e44e23a5fb9ef9a0f5
EdwardB911 <ebukhman@stuy.edu>
2017-11-28 21:09:07 -0500

    Finished Book

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -22,6 +22,26 @@ public class Book{
 	return title;
     }
 
+    public String getISBN(){
+	return ISBN;
+    }
+
+    public void setAuthor(String str){
+	author = str;
+    }
+
+    public void setTitle(String str){
+	title = str;
+    }
+
+    public void  setISBN(String str){
+	ISBN = str;
+    }
+
+    public String toString(){
+	return "" + this.getAuthor() + this.getTitle() + this.getISBN();
+    }
+
     
 }
 	    
