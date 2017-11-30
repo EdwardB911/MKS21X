00417300b3454f768ad7a9378a55b406fbdb96c0
EdwardB911 <ebukhman@stuy.edu>
2017-11-29 21:38:21 -0500

    added LibraryBook

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,34 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+
+    private String callNumber;
+
+    public LibraryBook(String a, String t, String i, String c){
+	super(a,t,i);
+	callNumber = c;
+    }
+
+    public String getCallNumber(){
+	return callNumber;
+    }
+
+    public void setCallNumber(String str){
+	callNumber = str;
+    }
+
+    public abstract void checkout(String patron, String due);
+
+    public abstract void returned();
+
+    public abstract String circulationStatus();
+
+    public int compareTo(LibraryBook other){
+	return (callNumber.compareTo(other.callNumber));
+    }
+
+    public String toString(){
+	return super.toString() + callNumber;
+    }
+}
+
+    
+	    
