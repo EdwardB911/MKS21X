b937aa82a0050664fde5964e73553dcbed79010c
EdwardB911 <ebukhman@stuy.edu>
2017-11-29 22:09:49 -0500

    Added reference books

---
+++ 05Library/ReferenceBook.java
@@ -0,0 +1,33 @@
+public class ReferenceBook extends LibraryBook{
+
+    private String collection;
+
+    public ReferenceBook(String a,String t,String i,String c,String co){
+	super(a,t,i,c);
+	collection = co;
+    }
+
+    public String getCollection(){
+	return collection;
+    }
+
+    public void setCollection(String str){
+	collection = str;
+    }
+
+    public void checkout(String patron, String due){
+	throw new RuntimeException("cannot check out a reference book");
+    }
+
+    public void returned(){
+	throw new RuntimeException("reference book could not have been checked out -- return impossible");
+    }
+
+    public String circulationStatus(){
+	return ("non-circulating reference book");
+    }
+
+    public String toString(){
+	return super.toString() + collection;
+    }
+}
