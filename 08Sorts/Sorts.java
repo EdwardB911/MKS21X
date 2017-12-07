2b97888ae629f06a65a4da333d9e6d357d73f8e2
EdwardB911 <ebukhman@stuy.edu>
2017-12-06 20:01:47 -0500

    began

---
+++ 08Sorts/Sorts.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;                                                            
+
+public class Sorts{                                                                 
+                                                                        
+
+    public static String name(){                                                    
+
+        return "10.Chen.Mohammad";                                                  
+
+    }                                                                               
+                                                                        
+
+    public static boolean isSorted(int[]ary){                                       
+
+        for(int i = 0; i < ary.length - 1 ; i++){                                   
+
+            if(ary[i] > ary[i+1]){                                                  
+
+                return false;                                                       
+
+            }                                                                       
+
+        }                                                                           
+
+        return true;                                                                
+
+    }                                                                               
+                                   
+
+    public static void bogoSort(int[] ary){                                         
+
+        while(!isSorted(ary)){                                                      
+
+            for(int i = 0 ; i < ary.length; i++){                                   
+
+                int temp = ary[i];                                                  
+
+                int newSpot = (int)(Math.random()*ary.length);                      
+
+                ary[i] = ary[newSpot];                                              
+
+                ary[newSpot] = temp;                                                
+
+            }                                                                       
+
+        }                                                                           
+
+    }                                                                               
+                                                                                   
+
+    private static void swap(int[]ary,int a, int b){                                
+
+        int c =ary[a];                                                              
+
+        ary[a] = ary[b];                                                            
+
+        ary[b] = c;                                                                 
+
+    }
+
+    public static void selectionSort
+}
